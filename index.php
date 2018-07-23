d2cdee1e18e82817d0659c84a4c52c94e3177fc0
PavelLapin <pavel-lapin@yandex.ru>
2018-07-23 16:11:05 +0300

    php index

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Date: 22.07.18
+ * Time: 19:56
+ */
+?>
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>test php7</title>
+</head>
+<body>
+Learning of PHP7.  Tests and  listings.
+<br>Use version <?php echo PHP_VERSION ?>
+</body>
+</html>
+
